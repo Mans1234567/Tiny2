b10d110373dd804ec490062c4fb864ea5a0409ea
Mansie Gee <mansiegee@gmail.com>
2025-12-04 21:17:08 +0100

    worked a bit more on the foundation

--- Tiny2.py
+++ Tiny2.py
@@ -1,2 +1,14 @@
-def render(matrix, system="windows"):
-    pass
+
+
+class Screen():
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+        self.screen = []
+        for y in range(0,self.height):
+            line = []
+            for x in range(0,self.width):
+                line.append(" ")
+
+    def render(self,matrix, system="windows"):
+        output = ""
