a91b837567eed44bb82c8d313b34cd7272cf80ae
Mansie Gee <mansiegee@gmail.com>
2025-12-05 22:26:09 +0100

    did the basic renderer

--- Tiny2.py
+++ Tiny2.py
@@ -5,10 +5,24 @@ class Screen():
         self.width = width
         self.height = height
         self.screen = []
+        self.previosly_rendered_screen = None
         for y in range(0,self.height):
             line = []
             for x in range(0,self.width):
-                line.append(" ")
+                line.append("█")
+            self.screen.append(line)
 
-    def render(self,matrix, system="windows"):
+    def render(self, system="windows"):
+        # implement efficiancies e.g. previosly renderd screen and only updating changes
         output = ""
+        for y in range(len(self.screen)):
+            for x in range(len(self.screen[y])):
+                output = output+str(self.screen[y][x])
+            output = output+"\n"
+
+        print(output, end="")
+
+
+s = Screen(20,10)
+
+s.render()
